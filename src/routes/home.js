f7ca83f0e47783518eeb5fb11d047aec6f2a5767
alancotal <acotal2018@inf.udec.cl>
2022-10-11 17:14:02 -0300

    Landing page v1.0
    Falta dar formato al texto e imagen. Además la parte inferior de la página

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,14 +7,40 @@ import {
   AccordionSummary,
   AccordionDetails,
   Typography,
+  CssBaseline,
+  Tooltip,
+  Button,
+  Stack,
 } from "@mui/material";
+import { positions } from "@mui/system";
+import landing from "../assets/img/landing.jpeg";
 import { ExpandMoreIcon } from "@mui/icons-material";
 const Home = () => {
   return (
     <div className="App">
-      <div className="container">
-        <span>¡Hola Mundo!</span>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className="container">
+          <div>
+            <h2>Universidad de Concepción</h2>
+            <h1>Toxicología de la alimentación</h1>
+            <h3>
+              Conocer la toxicidad que contienen los alimentos que consumes puede resultar en una
+              práctica positiva
+            </h3>
+            <div className="containerCenter">
+              <Stack direction="row" spacing={2}>
+                <Button variant="outlined" color="warning">
+                  Calculadora
+                </Button>
+                <Button variant="contained" color="warning">
+                  Administrador
+                </Button>
+              </Stack>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     </div>
   );
 };
